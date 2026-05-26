e19421fc1460f2011a6d8bd9c73876a652cf22f9
Fahid Skye <shadh56@hotmail.com>
2026-05-26 16:25:22 +0300

    Pos Module work

---
+++ FocusSalesModule/Logs/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Focus.Common.DataStructs;
+
+namespace FocusSalesModule.Logs
+{
+    public class Logger
+    {
+
+        public static void writeLog(string sContent)
+        {
+            FConvert.LogFile(DateTime.Now.ToString("MMyy") + "focussalesmodule.log", DateTime.Now.ToString() + " : " + sContent);
+
+        }
+
+
+
+    }
+}
